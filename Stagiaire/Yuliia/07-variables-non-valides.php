55cf84a722c22a6cdf94e98ccbc8931225d528ac
julia-dm <yulia.2508dmitruk@gmail.com>
2026-02-20 11:09:30 +0100

    exercise 8 done

---
+++ Stagiaire/Yuliia/07-variables-non-valides.php
@@ -0,0 +1,7 @@
+<?php
+$var str="hello world";
+$var-int=4;
+$1var_boolean=true;
+$var@float=3.14;
+var_name="Yuliia";
+var_dump($var_str,$var_int,$var_boolean,$var_float );
---
+++ Stagiaire/Yuliia/08-types.php
@@ -0,0 +1,13 @@
+<?php 
+$var_str="3.14";
+
+$entier = (int) $var_str;       // 3
+$decimal = (float) $var_str;    // 3.14
+$booleen = (bool) $var_str;     // true
+var_dump($var_str);
+var_dump($entier);
+var_dump($decimal);
+var_dump($booleen);
+
+
+
